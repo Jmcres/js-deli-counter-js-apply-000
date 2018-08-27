62af1ef2799468823a0322d1168fdd2589100a2c
Jason Crescenzo <crescenzojay@gmail.com>
2018-08-27 22:46:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ function currentLine (array){
    empty.push(" "+ (i+1) +". " + array[i]);
  }
  return "The line is currently:" + empty;
-} else {
+} 
+if (currentLine.length === 0){
   return "The line is currently empty.";
 }
 }
