6a4fc616116e7c4796ba0eb79418c7a44f4d9345
Jason Crescenzo <crescenzojay@gmail.com>
2018-08-27 22:47:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function nowServing(currentLine){
 }
 
 function currentLine (array){
+   var empty = [];
   if(currentLine.length !== 0){
-    var empty = [];
     for (var i = 0; i<array.length; i++){
    empty.push(" "+ (i+1) +". " + array[i]);
- }
  return "The line is currently:" + empty;
 } 
 if (currentLine.length === 0){
   return "The line is currently empty.";
+}
 }
 }
